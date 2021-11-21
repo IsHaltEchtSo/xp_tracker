b98174cca1deb680fc64ec44f0d78f7a51acfcd5
IsHaltEchtSo <d.grollmusz@gmail.com>
2021-11-21 19:50:51 +0100

    html frontend with mockup data

--- app/skill/routes.py
+++ app/skill/routes.py
@@ -1,19 +1,31 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, url_for
 
-skill = Blueprint('skill', __name__, url_prefix='/skills')
+skill = Blueprint(
+    'skill', __name__, url_prefix='/skills', template_folder='templates'
+)
 
 
 @skill.route('/')
-@skill.route('/overview')
-def skills_overview():
-    return 'the skills'
+@skill.route('/overview/graph')
+def skills_graph_overview():
+    return render_template('overview_graph.html')
 
 
-@skill.route('/skill/<skill>')
-def skill_page(skill):
-    return 'the page for the {} skill'.format(skill)
+@skill.route('/overview/history')
+def skills_history_overview():
+    return render_template('overview_history.html')
 
 
-@skill.route('/reward')
-def reward_page():
-    return 'pick your reward'
+@skill.route('/<skill>/history')
+def skill_page_historic(skill):
+    return render_template('skill-historic.html', skill=skill)
+
+
+@skill.route('/<skill>/graph')
+def skill_page_graphical(skill):
+    return render_template('skill-graphical.html', skill=skill)
+
+
+@skill.route('/<skill>/reward')
+def reward_page(skill):
+    return render_template('reward.html', skill=skill)
