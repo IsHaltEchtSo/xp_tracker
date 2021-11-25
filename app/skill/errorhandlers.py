158758019e503b86655cd38769ba0407fa975d98
IsHaltEchtSo <d.grollmusz@gmail.com>
2021-11-25 21:31:17 +0100

    construction message for the zk blueprint

--- app/skill/errorhandlers.py
+++ app/skill/errorhandlers.py
@@ -4,3 +4,7 @@ from flask import render_template
 @skill.app_errorhandler(404)
 def not_found(error):
     return render_template('404.html', error=error), 404
+
+@skill.route('/zk')
+def zk_page():
+    return render_template('zk.html')
